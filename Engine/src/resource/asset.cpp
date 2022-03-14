c829d6b4dd0c9ee685562e44385e83de189bb6ad
Truc N <81723190+yasmineerita@users.noreply.github.com>
2022-03-14 12:09:54 -0700

    Create asset.cpp

---
+++ Engine/src/resource/asset.cpp
@@ -0,0 +1,12 @@
+/****************************************************************************
+ * Copyright ©2017 Brian Curless.  All rights reserved.  Permission is hereby
+ * granted to students registered for University of Washington CSE 457 or CSE
+ * 557 for use solely during Autumn Quarter 2017 for purposes of the course.
+ * No other use, copying, distribution, or modification is permitted without
+ * prior written consent. Copyrights for third-party components of this work
+ * must be honored.  Instructors interested in reusing these course materials
+ * should contact the author.
+ ****************************************************************************/
+#include "asset.h"
+
+uint64_t Asset::uid_counter_ = 0;
